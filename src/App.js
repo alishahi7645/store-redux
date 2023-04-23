aa5d6ad21faa74ae416b9b34db224dd882f5b0cd
alishahi <shahy8227@gmail.com>
2023-04-23 12:53:24 -0700

    Project preparation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          تست
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
