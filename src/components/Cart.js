2fbb0f0319f7e836a062cd2ccd64774e472fd9ab
alishahi <shahy8227@gmail.com>
2023-05-05 02:53:28 -0700

    Show products in the shopping cart

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,10 +1,37 @@
 import React from "react";
 import Navbar from "./Navbar";
+import { Data } from "../data";
+import formatCurrency from "../util";
+import { useSelector, useDispatch } from "react-redux";
 
 function Cart() {
+  const cart = useSelector((store) => store);
+  const dispatch = useDispatch();
   return (
     <>
       <Navbar />
+      <div className="cart">
+        {cart.map((item) => {
+          return (
+            <div className="cart-item" key={item.id}>
+              <img src={item.image} alt="" />
+              <div className="cart-item-text">
+                <div className="cart-item-info">
+                  <h4>{item.title}</h4>
+                  <h5>{formatCurrency(item.price)}</h5>
+                </div>
+                <div className="add-to-cart">
+                  <button
+                    onClick={() => dispatch({ type: "ADD", payload: item })}
+                  >
+                    افزودن به سبد خرید
+                  </button>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
